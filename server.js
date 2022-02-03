a633dbfd683e0ff70eb17f16f4970113eb92d7e8
Abasiodiong Udofia <enalsy22@gmail.com>
2022-02-03 06:08:52 +0100

    readme updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.disable("x-powered-by");
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 app.use(
   express.urlencoded({
     limit: "50mb",
